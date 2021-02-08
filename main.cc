7548c09a7cb6fdce5f5317b617c056acf4c25bfe
Shea Levy <shea@shealevy.com>
2021-02-08 09:51:19 -0500

    ffid command scaffolding.

--- main.cc
+++ main.cc
@@ -1,29 +1,18 @@
 #include <config.h>
 #include <command.hh>
-#include "testsuite.hh"
 
 using namespace nix;
 
-struct CmdFci : NixMultiCommand
+struct CmdFfid : Command
 {
-    CmdFci() : MultiCommand({
-            { "testsuite", [](){ return make_ref<CmdTestsuite>(); } }
-        })
+    void run() override
     {
     }
 
     std::string description() override
     {
-        return "programmatic access to Nix primitives";
-    }
-
-    void run() override
-    {
-        if (!command)
-            throw UsageError("no fci subcommand specified");
-        command->second->prepare();
-        command->second->run();
+        return "the Nix FFI daemon";
     }
 };
 
-static auto r1 = registerCommand<CmdFci>("fci");
+static auto r1 = registerCommand<CmdFfid>("ffid");
