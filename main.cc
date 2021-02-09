061932175ed0c695976c0e1bc2e9685a4dc808bc
Shea Levy <shea@shealevy.com>
2021-02-09 12:35:33 -0500

    Add ADD_TEMPROOT command

--- main.cc
+++ main.cc
@@ -1,12 +1,39 @@
 #include <config.h>
 #include <command.hh>
 
+#define ADD_TEMPROOT 0
+
 using namespace nix;
 
-struct CmdFfid : Command
+const static char ok = '\0';
+
+class CmdFfid : public StoreCommand
 {
-    void run() override
+    FdSource in = STDIN_FILENO;
+    FdSink out = STDOUT_FILENO;
+
+    void run(ref<Store> store) override
     {
+        vector<char> buf(4096);
+        while (true) {
+            char command;
+            try {
+                in(&command, sizeof command);
+            } catch (EndOfFile &){
+                break;
+            }
+            if (command == ADD_TEMPROOT) {
+                size_t len;
+                in((char *) &len, sizeof len);
+                if (len > buf.size())
+                    buf.resize(len);
+                in(buf.data(), len);
+                store->addTempRoot(StorePath(std::string_view(buf.data(), len)));
+                out(std::string_view(&ok, sizeof ok));
+            } else {
+                throw Error("unknown command byte %c%", command);
+            }
+        }
     }
 
     std::string description() override
